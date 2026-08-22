68e95450c3741e46f548fc73e8b01974ca9c28a3
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-08-22 23:04:21 +0200

    more grid lines

--- res/shader/grid.fs
+++ res/shader/grid.fs
@@ -4,15 +4,22 @@ in vec3 b_Position;
 
 out vec4 o_Color;
 
+float gridalpha(vec2 c_Coord) {
+    vec2 c_Distance = abs(fract(c_Coord - 0.5f) - 0.5f);
+    vec2 c_Derivative = fwidth(c_Coord);
+    return max(
+        (1.0f - smoothstep(0.0f, c_Derivative.x, c_Distance.x)),
+        (1.0f - smoothstep(0.0f, c_Derivative.y, c_Distance.y))
+    );
+}
+
 void main() {
     vec2 c_Coord = b_Position.xz;
-    vec2 c_Grid = abs(fract(c_Coord - 0.5f) - 0.5f);
-    vec2 c_Derivative = fwidth(c_Coord);
 
-    float c_Alpha = max(
-        (1.0f - smoothstep(0.0f, c_Derivative.x, c_Grid.x)), 
-        (1.0f - smoothstep(0.0f, c_Derivative.y, c_Grid.y))
-    );
+    float c_PrimaryAlpha = gridalpha(c_Coord) * 0.8f;
+    float c_SecondaryAlpha = gridalpha(c_Coord * 10.0f) * (1.0f - c_PrimaryAlpha) * 0.2f;
+
+    float c_Alpha = max(c_PrimaryAlpha, c_SecondaryAlpha);
     c_Alpha *= max(0.0f, 1.0f - length(c_Coord) / 100.0f);
 
     o_Color = vec4(0.5f, 0.5f, 0.5f, c_Alpha);
