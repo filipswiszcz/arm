a73e39ee0dfcf957771a89394660da51e5f564fc
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-09-07 23:59:01 +0200

    add object and component

---
+++ src/component.h
@@ -0,0 +1,10 @@
+#ifndef ARM_COMPONENT_H
+
+class Component {
+public:
+    void initialize(void);
+    void update(void);
+    void destroy(void);
+};
+
+#endif // !ARM_COMPONENT_H
---
+++ src/object.h
@@ -0,0 +1,12 @@
+#ifndef ARM_OBJECT_H
+
+#include "renderer.h"
+
+class Object {
+public:
+private:
+    moID model;
+    Transform transform;
+};
+
+#endif // !ARM_OBJECT_H
